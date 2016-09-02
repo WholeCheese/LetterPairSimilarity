710a7065c7d00c55ff14a1f9b0c100055582c7f0
Allan Hoeltje <ahoeltje@lmi.net>
2016-09-02 15:47:18 -0700

    Initial Commit

---
+++ LetterPairSimilarity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LetterPairSimilarity
+//
+//  Created by Allan Hoeltje on 9/2/16.
+//  Copyright © 2016 Allan Hoeltje. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
